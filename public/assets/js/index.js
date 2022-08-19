4e2793b9455ec09803bfc31c7ce767a1b878c06f
idakukimiya <kukimiya.ida@gmail.com>
2022-08-19 00:43:23 -0400

    cleaning up code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -16,7 +16,7 @@ const hide = (elem) => {
 };
 
 // activeNote is used to keep track of the note in the textarea
-let activeNote = {};
+var activeNote = {};
 
 const getNotes = () =>
   fetch('/api/notes', {
